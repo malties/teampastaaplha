e49e32b362fcca6e7ac21835b56baeccd9fd4f2d
george <george_kar@hotmail.com>
2018-12-27 21:39:43 +0100

    Cost variance chart

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,45 +7,60 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
-public class costVarience extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
         //Defining the x axis
-        NumberAxis xAxis = new NumberAxis(10000, 20000, 1000);
-        xAxis.setLabel("cost");
+        NumberAxis xAxis = new NumberAxis (0, 8, 2);
+        xAxis.setLabel("Time in weeks");
 
         //Defining the y axis
-        NumberAxis yAxis = new NumberAxis   (0, 8, 2);
-        yAxis.setLabel("time");
+        NumberAxis yAxis = new NumberAxis  (100000, 800000, 100000);
+        yAxis.setLabel("Cost in SEK");
 
         //Creating the line chart
         LineChart linechart = new LineChart(xAxis, yAxis);
 
+
         //Prepare XYChart.Series objects by setting data
         XYChart.Series series = new XYChart.Series();
-        series.setName("Cost variance   ");
+        series.setName("EV");
 
-        series.getData().add(new XYChart.Data(10000, 1));
-        series.getData().add(new XYChart.Data(11000, 2));
-        series.getData().add(new XYChart.Data(13000, 3));
-        series.getData().add(new XYChart.Data(17000, 4));
-        series.getData().add(new XYChart.Data(19000, 5));
-        series.getData().add(new XYChart.Data(20000, 6));
+
+        series.getData().add(new XYChart.Data(0, 200000));
+        series.getData().add(new XYChart.Data(2, 300000));
+        series.getData().add(new XYChart.Data(4, 400000));
+        series.getData().add(new XYChart.Data(6, 400000));
+        series.getData().add(new XYChart.Data(8, 500000));
+
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("AC");
+
+        series2.getData().add(new XYChart.Data(0, 100000));
+        series2.getData().add(new XYChart.Data(2, 200000));
+        series2.getData().add(new XYChart.Data(4, 300000));
+        series2.getData().add(new XYChart.Data(6, 300000));
+        series2.getData().add(new XYChart.Data(8, 400000));
 
         //Setting the data to Line chart
-        linechart.getData().add(series);
+        linechart.getData().addAll(series,series2);
+
 
         //Creating a Group object
         Group root = new Group(linechart);
+
 
         //Creating a scene object
         Scene scene = new Scene(root, 600, 400);
 
         //Setting title to the Stage
-        stage.setTitle("Line Chart");
+        stage.setTitle("Cost Variance");
 
         //Adding scene to the stage
         stage.setScene(scene);
+
+
 
         //Displaying the contents of the stage
         stage.show();
