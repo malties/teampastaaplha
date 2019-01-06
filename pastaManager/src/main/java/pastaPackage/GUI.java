b7ab9894fd23cfc8e655072144a87bc8a605edbb
Fredrik Ullman <fredrikullman93@gmail.com>
2019-01-06 16:39:07 +0100

    three methods for Earned value, Schedule variance and cost variance

--- pastaManager/src/main/java/pastaPackage/GUI.java
+++ pastaManager/src/main/java/pastaPackage/GUI.java
@@ -26,8 +26,8 @@ public class GUI extends Application {
     Button printProject;
     Button costVarianceButton;
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
 
 
--- pastaManager/src/main/java/pastaPackage/Variance.java
+++ pastaManager/src/main/java/pastaPackage/Variance.java
@@ -1,16 +1,33 @@
 package pastaPackage;
 
 public class Variance {
+    private double earnedValue;
+    private double scheduleVariance;
+    private double costVariance;
+    private static final int TOTAL_BUDGET = 1134840;
+    private static final int PLANNED_SPEND = 1100000;
+    private static final int ACTUAL_SPEND = 1200000;
 
-    public void calculateEV(){
-        // TODO
+
+    public Variance(double earnedValue, double scheduleVariance, double costVariance) {
+        this.earnedValue = earnedValue;
+        this.scheduleVariance = scheduleVariance;
+        this.costVariance = costVariance;
+
+    }
+
+    public double calculateEV(double percentage){
+        this.earnedValue = TOTAL_BUDGET * (percentage/100);
+        return this.earnedValue;
     }
 
     public void calculateSV(){
-        // TODO
+     this.scheduleVariance = earnedValue - PLANNED_SPEND;
     }
 
     public void calculateCV(){
-        // TODO
+        this.costVariance = earnedValue - ACTUAL_SPEND;
+
+
     }
 }
