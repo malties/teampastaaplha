75980e7f07b3d045460c44009c194894638e5332
Gusmuaha <43820542+Gusmuaha@users.noreply.github.com>
2019-01-17 10:27:36 +0100

    had to push here since my bash crashed on me
    Improved graphs for GUI

---
+++ teampastaaplha/pastaManager/src/main/java/pastaPackage/ProjectData.java
@@ -0,0 +1,109 @@
+package pastaPackage;
+
+import java.util.ArrayList;
+
+public class ProjectData {
+
+    private String PName;
+    private String PId;
+    private int startWeek;
+    private int endWeek;
+    private int startYear;
+    private int endYear;
+    private double budget;
+    private ArrayList<Integer> additionalCosts;
+    private double additionalCost;
+
+    /*
+    private int additionalCostQ1;
+    private int additionalCostQ2;
+    private int additionalCostQ3;
+    private int additionalCostQ4;
+*/
+
+    public String getPName() {
+        return PName;
+    }
+
+    public void setPName(String PName) {
+        this.PName = PName;
+    }
+
+    public String getPId() {
+        return PId;
+    }
+
+    public void setPId(String PId) {
+        this.PId = PId;
+    }
+
+    public int getStartWeek() {
+        return startWeek;
+    }
+
+    public void setStartWeek(int startWeek) {
+        this.startWeek = startWeek;
+    }
+
+    public int getEndWeek() {
+        return endWeek;
+    }
+
+    public void setEndWeek(int endWeek) {
+        this.endWeek = endWeek;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(int startYear) {
+        this.startYear = startYear;
+    }
+
+    public int getEndYear() { return endYear; }
+
+    public void setEndYear(int endYear) {
+        this.endYear = endYear;
+    }
+
+    public double getBudget() { return budget; }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public double getAdditionalCost(double interval) {
+        return this.additionalCost;
+    }
+
+    public void setAdditionalCost(int additionalCost) {
+        this.additionalCost = additionalCost;
+    }
+
+    public ArrayList<Integer> getAdditionalCosts() {
+        return additionalCosts;
+    }
+
+    public void setAdditionalCosts(ArrayList<Integer> additionalCosts) {
+        this.additionalCosts = additionalCosts;
+    }
+
+    /*
+    public int getAdditionalCostQ1(){ return additionalCostQ1; }
+
+    public void setAdditionalCostQ1(int additionalCostQ1){ this.additionalCostQ1 = additionalCostQ1; }
+
+    public int getAdditionalCostQ2(){ return additionalCostQ2; }
+
+    public void setAdditionalCostQ2(int additionalCostQ2){ this.additionalCostQ1 = additionalCostQ2; }
+
+    public int getAdditionalCostQ3(){ return additionalCostQ3; }
+
+    public void setAdditionalCostQ3(int additionalCostQ3){ this.additionalCostQ3 = additionalCostQ3; }
+
+    public int getAdditionalCostQ4(){ return additionalCostQ4; }
+
+    public void setAdditionalCostQ4(int additionalCostQ4){ this.additionalCostQ4 = additionalCostQ4; }
+    */
+}
